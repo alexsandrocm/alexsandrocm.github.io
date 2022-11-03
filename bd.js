ac2c45b49f3e0003cf6b0432cd7d22301b0e89fb
alexsandrocm <alexsandrotecn@gmail.com>
2022-11-03 11:58:40 -0300

    alteração site

--- bd.js
+++ bd.js
@@ -1,15 +1,16 @@
 var bd = openDatabase("meuBD", "1.0", "Meu Banco de Dados", 4080);
 
 bd.transaction(function (criar) {
-    criar.executeSql("CREATE TABLE formulario (nome TEXT)");
+    criar.executeSql("CREATE TABLE formulario (nome TEXT, idade INTEGER)");
 });
 
 function salvarNome() {
     const nomeUsuario = document.getElementById("nome-usuario").value;
-
+    const idadeUsuario = parseInt (document.getElementById("idade-usuario").value
+    );
 bd.transaction(function(inserir){
     inserir.executeSql(
-        "INSERT INTO formulario (nome) VALUES (?)", [nomeUsuario]
+        "INSERT INTO formulario (nome, idade) VALUES (?, ?)", [nomeUsuario, idadeUsuario]
     );
 });
 }
