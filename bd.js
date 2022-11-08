77d8f2b2d56ad417787b48ab8c3ad9179895dd6d
alexsandro_cm <alexsandrotecn@gmail.com>
2022-11-08 11:51:52 -0300

    Edição no banco de dados

--- bd.js
+++ bd.js
@@ -5,23 +5,55 @@ bd.transaction(function (criar) {
 });
 
 function salvarInfo() {
-    const nomeUsuario = document.getElementById("nome-usuario").value;
+    const nomeUsuario = document.getElementById("nome-usuario").value.toUpperCase();
     const idadeUsuario = parseInt (document.getElementById("idade-usuario").value
     );
+
+    if (nomeUsuario === ""|| isNaN(idadeUsuario)){
+        alert ("Faltam informações!")
+        return false;
+    }
+
 bd.transaction(function(inserir){
     inserir.executeSql(
         "INSERT INTO formulario (nome, idade) VALUES (?, ?)", [nomeUsuario, idadeUsuario]
     );
 });
+
+document.getElementById("nome-usuario").value = "";
+document.getElementById("idade-usuario").value = "";
+
 }
 
 function pesquisaPorNome(){
-    const nomeUsuario = document.getElementById("pesquisa-nome-usuario").value;
+    const nomeUsuario = document.getElementById("pesquisa-nome-usuario").value.toUpperCase();
     bd.transaction(function (ler){
-        ler.executeSql(`SELECT * FROM formulario WHERE nome="${nomeUsuario}"`,[], function(ler, resultados){
+        ler.executeSql(`SELECT * FROM formulario WHERE nome LIKE "%${nomeUsuario}%"`,[], function(ler, resultados){
             const tamanho = resultados.rows.length;
             const msg = tamanho + "linhas encontradas";
             console.log(msg);
+
+            const nome = resultados.rows.item(tamanho - 1).nome;
+            const idade = resultados.rows.item(tamanho - 1).idade;
+            document.getElementById("pesquisa-nome-usuario").value = nome;
+            document.getElementById("resultado-pesquisa").value = idade;
+
         })
     });
+}
+
+function exibeBD(){
+    bd.transaction(function(exibe){
+        exibe.executeSql("SELECT * FROM formulario", [], function(exibe, resultados){
+            const tamanho = resultados.rows.length;
+            let item;
+
+            document.getElementById("lista-bd").innerHTML = "";
+            
+            for (let i = 0; i < tamanho; i++){
+                item = resultados.rows.item(i);
+                document.getElementById("lista-bd").innerHTML += `<p>Nome: ${item.nome}, ${item.idade} anos</p>` 
+            }
+        })
+    })
 }
